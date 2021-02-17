ff3732105c770963c5f63244e9e14398e4028c11
Ludvig Larsson <ludvig.larsson@scilifelab.se>
2021-02-17 15:55:03 +0100

    Added answers for day 2 and day 3

---
+++ day2/buggy/dicegame/runner.py
@@ -0,0 +1,76 @@
+from .die import Die
+#from .utils import i_just_throw_an_exception
+
+class GameRunner:
+
+    def __init__(self):
+        self.dice = Die.create_dice(5)
+        self.reset()
+
+    def reset(self):
+        self.round = 1
+        self.wins = 0
+        self.loses = 0
+
+    def answer(self):
+        total = 0
+        for die in self.dice:
+            total += die.value
+        return total
+
+    @classmethod
+    def run(cls):
+        # Probably counts wins or something.
+        # Great variable name, 10/10.
+        c = 0
+        runner = cls()
+        while True:
+
+            print("Round {}\n".format(runner.round))
+
+            for die in runner.dice:
+                print(die.show())
+            
+            while True:
+                guess = input("Sigh. What is your guess?: ")
+                try:
+                    guess = int(guess)
+                    print("Your guess: %s" % guess)
+                    break
+                except:
+                    print("Invalid guess %s" % str(guess))
+                    continue
+
+            if guess == runner.answer():
+                print("Congrats, you can add like a 5 year old...")
+                runner.wins += 1
+                c += 1
+            else:
+                print("Sorry that's wrong")
+                print("The answer is: {}".format(runner.answer()))
+                print("Like seriously, how could you mess that up")
+                runner.loses += 1
+            print("Wins: {} Loses {}".format(runner.wins, runner.loses))
+            runner.round += 1
+
+            if c == 6:
+                print("You won... Congrats...")
+                print("The fact it took you so long is pretty sad")
+                break
+            
+            while True:
+                prompt = input("Would you like to play again?[Y/n]: ")
+                if prompt in ['Y', '', 'n']:
+                    break
+                else:
+                    print("Invalid option %s" % str(prompt))
+                    continue
+                
+            if prompt == 'Y' or prompt == '':
+                # Update dice
+                runner.dice = Die.create_dice(5)
+                continue
+            elif prompt == 'n':
+                break
+            
+                
